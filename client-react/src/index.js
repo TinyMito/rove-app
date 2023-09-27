e193c122590bd246536c4676ab916041d198ad4e
eunsookim1 <92eunsoo@gmail.com>
2023-09-26 19:21:47 -0700

    create a way to have multiple paths

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 import App from "App";
 import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'));
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root'));
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App />
+    ),
+  },
+  {
+    path: "example",
+    element: <div>Example</div>,
+  },
+  {
+    path: "schedule_day",
+    element: <Schedule />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
