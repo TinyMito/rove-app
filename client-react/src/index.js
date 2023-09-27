57e0ed309e7fcae0ee07163688186322b46b0dc6
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-09-26 23:22:22 -0400

    Remove App and refactor to Index

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "App";
-import './index.css';
+import { createRoot } from 'react-dom/client'; // React ^18
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Temporary
+import './index.scss';
+import Home from "./components"; // Temporary
+import Developer from "./components/DevCss"; // Temporary
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'));
+const rootElement = document.getElementById('root'); // React ^18
+const root = createRoot(rootElement); // React ^18
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={ <Home/> } />
+      <Route path="/dev" element={ <Developer/> } /> {/* Temporary using BrowserRoute for Dev purpose (not SPA compliance) */ }
+    </Routes>
+  </BrowserRouter>
+);
