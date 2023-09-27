b594787e3fb05d50d05677ddcec15a9ed616ba9a
eunsookim1 <92eunsoo@gmail.com>
2023-09-26 22:19:09 -0700

    update layout

--- client-react/src/components/Schedule.jsx
+++ client-react/src/components/Schedule.jsx
@@ -4,31 +4,98 @@ export default function Schedule() {
       <title>Vancouver</title>
       <div id="root"></div>
 
-      <h1>Vancouver</h1>
+      <h1 className="trip_location">Vancouver</h1>
 
       <h2>Day: 1 2 3 4 5</h2>
       <h3 className="travel_dates">2023.10.12-2023.10.16</h3>
-      <section className="timeline">
-        <div>8:00</div>
-        <div>9:00</div>
-        <div className="travel_time_direction">15 min Direction</div>
-        <div>10:15</div>
-        <div>11:00</div>
-        {/* <!-- how to match the timeline with the photo? --> */}
+      <div className="timeline" />
 
-      </section>
       <section className="itinerary_day">
-        <div>
-          <img className="place_url" />
+        <div className="add">
+          <i class="bi bi-plus"></i>
+        </div>
 
-          <span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-map" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M15.817.113A.5.5 0 0 1 16 .5v14a.5.5 0 0 1-.402.49l-5 1a.502.502 0 0 1-.196 0L5.5 15.01l-4.902.98A.5.5 0 0 1 0 15.5v-14a.5.5 0 0 1 .402-.49l5-1a.5.5 0 0 1 .196 0L10.5.99l4.902-.98a.5.5 0 0 1 .415.103zM10 1.91l-4-.8v12.98l4 .8V1.91zm1 12.98 4-.8V1.11l-4 .8v12.98zm-6-.8V1.11l-4 .8v12.98l4-.8z" />
-            </svg>
-          </span>
+        <div className="place_card_thumbnail">
+          <h3>Fairmont Hotel</h3>
 
+          <section className="time">
+            <div>8:00</div>
+          </section>
+          <i class="bi bi-arrows-fullscreen"></i>
+          <img className="place_url" src="https://tinyurl.com/3jskbrps"width="400"/>
+          <i class="bi bi-map"></i>
+          <i class="bi bi-trash"></i>
+        </div>
+
+        <article className="place_card">
+          <h3>Stanley Park</h3>
+
+          <section className="time">
+            <div className="travel_time_direction">
+              <i class="bi bi-car-front"></i>
+              <div>10 min </div> 
+              <a>Direction</a>
+            </div>
+            <div>8:10</div>
+            <div>10:00</div>
+          </section>
+
+          <section>
+            <i class="bi bi-arrows-fullscreen"></i>
+            <img className="place_url" src="https://tinyurl.com/ta4kmc28"width="400"/>
+            <i class="bi bi-map"></i>
+            <i class="bi bi-trash"></i>
+          </section>
+          
+        </article>
+
+        <div className="place_card_thumbnail">
+          <h3>English Bay</h3>
+
+          <section className="time">
+            <div className="travel_time_direction">
+              <i class="bi bi-car-front"></i>
+              <div>10 min </div> 
+              <a>Direction</a>
+            </div>
+            <div>10:10</div>
+            <div>11:45</div>
+          </section>
+
+          <i class="bi bi-arrows-fullscreen"></i>
+          <img className="place_url" src="https://tinyurl.com/mrxhat82"width="400"/>
+          <i class="bi bi-map"></i>
+          <i class="bi bi-trash"></i>
+        </div>
+
+        <div className="place_card_thumbnail">
+          <h3>Chambar</h3>
+
+          <section className="time">
+            <div className="travel_time_direction">
+              <i class="bi bi-car-front"></i>
+              <div>15 min </div> 
+              <a>Direction</a>
+            </div>
+            <div>12:00</div>
+            <div>1:00</div>
+          </section>
+
+          <i class="bi bi-arrows-fullscreen"></i>
+          <img className="place_url" src="https://tinyurl.com/283e4nvt"width="400"/>
+          <i class="bi bi-map"></i>
+          <i class="bi bi-trash"></i>
+        </div>
+
+        <div className="place_card_thumbnail">
+          <h3>Suspension Bridge</h3>
+          <i class="bi bi-arrows-fullscreen"></i>
+          <img className="place_url" src="https://tinyurl.com/4cfv4uud"width="400"/>
+          <i class="bi bi-map"></i>
+          <i class="bi bi-trash"></i>
         </div>
       </section>
+      
     </div>
   );
 
