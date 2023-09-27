28965ed7df7ebb4ce059b28006c101df9678e03f
eunsookim1 <92eunsoo@gmail.com>
2023-09-26 19:50:17 -0700

    add layout

--- client-react/src/component/schedule.js
+++ client-react/src/component/schedule.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "App";
-import './index.css';
+import '../index.css';
 
 function Schedule() {
   return (
-   
+    <div>
+      <title>Vancouver</title>
+
+      <body>
+        <div id="root"></div>
+
+        <h1>Vancouver</h1>
+
+        <h2>Day: 1 2 3 4 5</h2>
+        <section class="timeline">
+          <div>8:00</div>
+          <div>9:00</div>
+          {/* <!-- how to match the timeline with the photo? --> */}
+
+        </section>
+        <section class="itinerary_day">
+          <div>
+            <img class="place_url" />
+            <span>map logo</span>
+          </div>
+        </section>
+
+
+      </body>
+    </div>
   );
 }
 
