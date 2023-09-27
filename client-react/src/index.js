c24f48cc82d9f7af21852aded848d2b9df4e94fc
Ahmed <ahmed91alhaj@gmail.com>
2023-09-27 07:33:00 -0400

    Add duration page

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -10,19 +10,21 @@ import Developer from './components/DevCss'; // Temporary
 import Google from './components/Autocomplete';
 import Suggestion from './components/Card';
 import Detail from './components/Detail';
+import Duration from './components/Duration'
 
 const rootElement = document.getElementById('root'); // React ^18
 const root = createRoot(rootElement); // React ^18
 root.render(
   <BrowserRouter>
-    <Global styles={ baseCss } />
+    <Global styles={baseCss} />
     <Routes>
-      <Route path="/" element={ <Application/> } />
-      <Route path="/schedule" element={ <Schedule /> } />
-      <Route path="/dev" element={ <Developer/> } /> // Temporary
-      <Route path="/google" element={ <Google />} />
-      <Route path="/card" element={ <Suggestion />} />
-      <Route path="/detail/:id" element={ <Detail />} />
+      <Route path="/" element={<Application />} />
+      <Route path="/schedule" element={<Schedule />} />
+      <Route path="/dev" element={<Developer />} /> // Temporary
+      <Route path="/google" element={<Google />} />
+      <Route path="/card" element={<Suggestion />} />
+      <Route path="/detail/:id" element={<Detail />} />
+      <Route path="/duration" element={<Duration />} />
     </Routes>
   </BrowserRouter>
 );
