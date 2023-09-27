f07cdf06c2e39c15951161cbd559f2991ccd0ec4
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-09-26 23:59:26 -0400

    Rename scheduler

--- client-react/src/components/Schedule.jsx
+++ client-react/src/components/Schedule.jsx
@@ -1,36 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "App";
-import '../index.css';
-
-function Schedule() {
+export default function Schedule() {
   return (
     <div>
       <title>Vancouver</title>
-
-      <body>
         <div id="root"></div>
 
         <h1>Vancouver</h1>
 
         <h2>Day: 1 2 3 4 5</h2>
-        <section class="timeline">
+        <section className="timeline">
           <div>8:00</div>
           <div>9:00</div>
           {/* <!-- how to match the timeline with the photo? --> */}
 
         </section>
-        <section class="itinerary_day">
+        <section className="itinerary_day">
           <div>
-            <img class="place_url" />
+            <img className="place_url" />
             <span>map logo</span>
           </div>
         </section>
-
-
-      </body>
     </div>
   );
 }
-
-export default Schedule;
