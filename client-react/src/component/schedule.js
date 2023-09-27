130c3e3fedc51dc1f061cd215d4ac30b7e4d0651
eunsookim1 <92eunsoo@gmail.com>
2023-09-26 19:23:23 -0700

    install react-router-dom

---
+++ client-react/src/component/schedule.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "App";
+import './index.css';
+
+function Schedule() {
+  return (
+   
+  );
+}
+
+export default Schedule;
