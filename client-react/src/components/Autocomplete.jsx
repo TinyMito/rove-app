d22f5e1f9487ce9031dc87393c658eca3bc92c79
Ahmed <ahmed91alhaj@gmail.com>
2023-09-30 13:09:26 -0400

    Merge main into Duration

--- client-react/src/components/Autocomplete.jsx
+++ client-react/src/components/Autocomplete.jsx
@@ -31,7 +31,7 @@ export default function GoogleAutocomplete() {
   };
 
   return (
-    <div className="App">
+    <div className="body">
       <PlacesAutocomplete
         value={address}
         onChange={setAddress}
--- client-react/src/index.js
+++ client-react/src/index.js
@@ -2,11 +2,17 @@ import { createRoot } from 'react-dom/client'; // React ^18
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Temporary
 import { Global } from '@emotion/react';
 import { baseCss } from './styles/baseStyles';
-import './index.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import Application from './components/Application';
+import Navigation from './components/partials/Navigation';
+import Header from './components/partials/Header';
+
 import { Schedule } from './components/Schedule/Schedule';
-import Developer from './components/DevCss'; // Temporary
 import Google from './components/Autocomplete';
 import Suggestion from './components/Card';
 import Place from './components/Place/Place';
@@ -15,18 +21,24 @@ import User from './components/User/User';
 
 const rootElement = document.getElementById('root'); // React ^18
 const root = createRoot(rootElement); // React ^18
+
 root.render(
   <BrowserRouter>
     <Global styles={baseCss} />
-    <Routes>
-      <Route path="/" element={<Application />} />
-      <Route path="/schedule/:id" element={<Schedule />} />
-      <Route path="/dev" element={<Developer />} />
-      <Route path="/google" element={<Google />} />
-      <Route path="/card/:location/:id" element={<Suggestion />} />
-      <Route path="/duration" element={<Duration />} />
-      <Route path="/place/:id" element={<Place />} />
-      <Route path="/user/:id" element={<User />} />
-    </Routes>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <Navigation />
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Application />} />
+        <Route path="/schedule/:id" element={<Schedule />} />
+        <Route path="/google" element={<Google />} />
+        <Route path="/card/:location/:id" element={<Suggestion />} />
+        <Route path="/duration" element={<Duration />} />
+        <Route path="/place/:id" element={<Place />} />
+        <Route path="/user/:id" element={<User />} />
+      </Routes>
+      </div>
+    </div>
   </BrowserRouter>
 );
