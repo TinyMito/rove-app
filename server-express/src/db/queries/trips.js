d24a4471744f26c11f709c7005fea9f8ba32329e
eunsookim1 <92eunsoo@gmail.com>
2023-09-30 15:08:02 -0700

    organize folders

--- server-express/src/db/queries/trips.js
+++ server-express/src/db/queries/trips.js
@@ -1,15 +1,46 @@
-// place, trip, destination
+const db = require('../connection');
 
-const db = require("../connection.js");
+const dbQuery = function (query) {
+  console.log(`Query: ${query.string}`);
+  return (
+    db.query(query.string, query.params)
+      .then(res => res.rows)
+  );
+};
 
-const getTrip = ("/api/schedule", (req, res) => {
-  return db.query(userTripSchedule)
-    .then(data => {
-      return data.rows;
-    })
-    .catch((err) => {
-      console.error(err.message);
-    });
-});
+const activitiesQuery =
+  `SELECT
+    T.id AS trip_id,
+    T.place_id AS place_id,
+    T.user_id,
+    T.schedule_id,
+    T.date,
+    T.start_time,
+    T.end_time, 
+    P.description AS place_description,
+    P.thumbnail_img_url,
+    P.cover_photo_url,
+    P.google_map_link
+  FROM
+    trips T
+  JOIN
+    places P
+      ON T.place_id = P.id
+  WHERE
+    T.schedule_id = $1
+    AND
+    T.date = $2
+  ORDER BY
+    T.start_time;`;
 
-module.exports = { getTrip };
+const getAllDayActivities = (scheduleId, date) => {
+  const query = {
+    string: activitiesQuery,
+    params: [scheduleId, date],
+  };
+  return dbQuery(query);
+};
+
+module.exports = {
+  getAllDayActivities,
+};
