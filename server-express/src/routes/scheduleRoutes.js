70e09a3d5f4afa34f74c898718da8fc8de26ff47
eunsookim1 <92eunsoo@gmail.com>
2023-10-01 00:27:15 -0700

    organize writeup

--- server-express/src/routes/scheduleRoutes.js
+++ server-express/src/routes/scheduleRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { getDestinationNDates } = require("../db/queries/schedule");
-
+// const { getAllDayTrips } = require("../db/queries/trip");
 router.get("/", (req, res) => {
   res.json({ test: 'ok' });
 });
@@ -11,10 +11,15 @@ router.get("/", (req, res) => {
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
 
-  getDestinationNDates(id)
-    .then((response) => {
-      res.json(response);
-    });
+  try {
+    getDestinationNDates(id)
+      .then((schedule) => {
+        res.json(schedule);
+      });
+  } catch (error) {
+    console.error('Error fetching data for schedule/:id:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 module.exports = router;
