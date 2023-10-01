7052a3bc15abb9741444f60765d537627e9f9119
Ahmed <ahmed91alhaj@gmail.com>
2023-10-01 17:53:42 -0400

    Implement login & register auth for users

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -18,6 +18,9 @@ import Suggestion from './components/Card';
 import Place from './components/Place/Place';
 import Duration from './components/Duration';
 import User from './components/User/User';
+import { Login } from '@mui/icons-material';
+import RegistrationForm from 'components/RegistrationForm';
+import LoginForm from 'components/LoginForm';
 
 const rootElement = document.getElementById('root'); // React ^18
 const root = createRoot(rootElement); // React ^18
@@ -28,16 +31,18 @@ root.render(
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <Navigation />
       <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Application />} />
-        <Route path="/schedule/:id" element={<Schedule />} />
-        <Route path="/google" element={<Google />} />
-        <Route path="/card/:location/:id" element={<Suggestion />} />
-        <Route path="/duration" element={<Duration />} />
-        <Route path="/place/:id" element={<Place />} />
-        <Route path="/user/:id" element={<User />} />
-      </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Application />} />
+          <Route path="/schedule/:id" element={<Schedule />} />
+          <Route path="/google" element={<Google />} />
+          <Route path="/card/:location/:id" element={<Suggestion />} />
+          <Route path="/duration" element={<Duration />} />
+          <Route path="/place/:id" element={<Place />} />
+          <Route path="/user/:id" element={<User />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegistrationForm />} />
+        </Routes>
       </div>
     </div>
   </BrowserRouter>
