ff21174392aba42a831b7eaf244b742c63646bc8
eunsookim1 <92eunsoo@gmail.com>
2023-10-02 14:13:12 -0700

    update

--- client-react/src/components/Schedule/useSchedule.js
+++ client-react/src/components/Schedule/useSchedule.js
@@ -1,20 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 export const useSchedule = ({ id }) => {
   const [schedule, setSchedule] = useState(null);
-  const [date, setDate] = useState(null);
+  const [dates, setDates] = useState([]);
+  const [currentDay, setCurrentDay] = useState(0);
 
   useEffect(() => {
-    fetch(`api/schedule/${id}`)
+    fetch(`/api/schedule/${id}`)
       .then((response) => {
         return response.json()
       })
       .then((response) => {
-        setSchedule(response)
-
+        const newSchedule = response[0];
+        setSchedule(newSchedule)
+        setDates([newSchedule.start_date, newSchedule.end_date])
       })
   }, [id])
 
-  return { schedule };
+  const handleSetDay = useCallback((event, dayIndex) => {
+    setCurrentDay(dayIndex - 1);
+  }, []);
+
+  return { schedule, dates, handleSetDay, currentDay, totalDays: dates.length };
 };
 
--- client-react/src/components/Schedule/useTimeLine.js
+++ client-react/src/components/Schedule/useTimeLine.js
@@ -1,19 +1,19 @@
 import { useState, useEffect, useCallback } from 'react';
 
 export const useTimeLine = ({ id, date }) => {
+
   const [data, setData] = useState([]);
 
-  // useEffect(() => {
-  //   setData(mockData[date])
-  // }, [date]);
   useEffect(() => {
-    fetch(`api/trip?scheduleId=${id}&date=${date}`)
-      .then((response) => {
-        return response.json()
-      })
-      .then((response) => {
-        setData(response)
-      })
+    if (date) {
+      fetch(`/api/trip?scheduleId=${id}&date=${date}`)
+        .then((response) => {
+          return response.json()
+        })
+        .then((response) => {
+          setData(response)
+        })
+    }
   }, [id, date])
   return { data };
 };
