ef72df608feba09a8178994167271db31da71a44
eunsookim1 <92eunsoo@gmail.com>
2023-10-02 21:22:32 -0700

    link current day to pagination

--- client-react/src/components/Schedule/useSchedule.js
+++ client-react/src/components/Schedule/useSchedule.js
@@ -1,21 +1,37 @@
 import { useState, useEffect, useCallback } from 'react';
+import { daysArray } from './utils';
 
 export const useSchedule = ({ id }) => {
   const [schedule, setSchedule] = useState(null);
-  const [dates, setDates] = useState([]);
+  const [dates, setDates] = useState([]); // 
   const [currentDay, setCurrentDay] = useState(0);
 
   useEffect(() => {
     fetch(`/api/schedule/${id}`)
+      .then((response) => response.json())
       .then((response) => {
-        return response.json()
-      })
-      .then((response) => {
-        const newSchedule = response[0];
-        setSchedule(newSchedule)
-        setDates([newSchedule.start_date, newSchedule.end_date])
-      })
-  }, [id])
+        const newSchedule = response[currentDay];
+        setSchedule(newSchedule);
+
+        // Assuming start_date and end_date are properties of newSchedule
+        const start_date = newSchedule.start_date;
+        const end_date = newSchedule.end_date;
+
+        // Pass start_date and end_date to daysArray
+        setDates(daysArray(start_date, end_date));
+      });
+  }, [id]);
+
+  //     .then((response) => {
+  //       return response.json()
+  //     })
+  //     .then((response) => {
+  //       const newSchedule = response[0];
+  //       setSchedule(newSchedule)
+  //       setDates(daysArray(start_date, end_date))
+
+  //     })
+  // }, [id, start_date, end_date])
 
   const handleSetDay = useCallback((event, dayIndex) => {
     setCurrentDay(dayIndex - 1);
