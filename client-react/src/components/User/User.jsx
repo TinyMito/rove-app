0cfd1aac6cb635a6a53345dab5f42278e0a70c9d
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-10-03 23:48:32 -0400

    Adapt the new design

--- client-react/src/components/User/User.jsx
+++ client-react/src/components/User/User.jsx
@@ -10,9 +10,9 @@ export default function Detail() {
   const { id } = useParams();
   const [place, setPlace] = useState({});
 
-   // Responsive Design for Mobile
-  const isMobile = useMediaQuery('(min-width:800px)');
-  const cardMaxWidth = isMobile ? 600 : 'auto';
+  // Responsive Design for Mobile
+  //const isMobile = useMediaQuery('(min-width:800px)');
+  //const cardMaxWidth = isMobile ? 600 : 'auto';
 
   useEffect(() => {
     const apiUser = `/api/user/${id}`;
@@ -27,41 +27,16 @@ export default function Detail() {
   }, [id]);
   
   return (
-    <Card sx={{ maxWidth: cardMaxWidth, padding: '0px 20px', border: '3px solid #ccc', borderRadius: 2 }}>
-      <Grid container spacing={5}>
-        <Grid item xs={12} md={8}>
-          <Typography sx={{ padding: '20px 0px' }} align="left" gutterBottom variant="h5" component="div">
-            {place.place_name}
-          </Typography>
+    <div className="body">
+      <Card sx={{ padding: '0px 20px'}}>
+        <Grid container spacing={5}>
+          <Grid item xs={12} md={8}>
+            <Typography sx={{ padding: '20px 0px' }} align="left" gutterBottom variant="h5" component="div">
+              {place.place_name}
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <CardActions >
-            <Button fullWidth={true} sx={{ fontSize: '30px' }} href={place.google_map_link} size="small" target="_blank"><i className="bi bi-map"></i></Button>
-            <Button fullWidth={true} sx={{ fontSize: '30px' }} href="#" size="small"><i className="bi bi-calendar-plus"></i></Button>
-          </CardActions>
-        </Grid>
-      </Grid>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={8}>
-          <CardMedia
-            component="img"
-            sx={{ height: 300, border: '3px solid #ccc', borderRadius: 2 }}
-            alt={place.place_name}
-            image={place.cover_photo_url}
-            title={place.place_name}
-          />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Typography align="left" variant="body2" color="text.secondary">
-            <Typography component="legend">Google Review Stars:</Typography>
-          </Typography>
-        </Grid>
-      </Grid>
-      <CardContent>
-        <Typography align="left" variant="body2" color="text.secondary">
-          {place.description}
-        </Typography>
-      </CardContent>
-    </Card>
+      </Card>
+    </div>
   );
 }
