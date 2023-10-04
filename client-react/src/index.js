38aac58b057a53c45dd9481ea8fa8df1d677a74c
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-10-03 23:47:36 -0400

    Remove dev, add layout css

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -9,8 +9,10 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import Application from './components/Application';
+import Navigation from './components/partials/Navigation';
+import Header from './components/partials/Header';
+
 import { Schedule } from './components/Schedule/Schedule';
-import Developer from './components/DevCss'; // Temporary
 import Google from './components/Autocomplete';
 import Suggestion from './components/Card';
 import Place from './components/Place/Place';
@@ -19,18 +21,24 @@ import User from './components/User/User';
 
 const rootElement = document.getElementById('root'); // React ^18
 const root = createRoot(rootElement); // React ^18
+
 root.render(
   <BrowserRouter>
     <Global styles={baseCss} />
-    <Routes>
-      <Route path="/" element={<Application />} />
-      <Route path="/schedule/:id" element={<Schedule />} />
-      <Route path="/dev" element={<Developer />} />
-      <Route path="/google" element={<Google />} />
-      <Route path="/card/:location/:id" element={<Suggestion />} />
-      <Route path="/duration" element={<Duration />} />
-      <Route path="/place/:id" element={<Place />} />
-      <Route path="/user/:id" element={<User />} />
-    </Routes>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <Navigation />
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Application />} />
+        <Route path="/schedule/:id" element={<Schedule />} />
+        <Route path="/google" element={<Google />} />
+        <Route path="/card/:location/:id" element={<Suggestion />} />
+        <Route path="/duration" element={<Duration />} />
+        <Route path="/place/:id" element={<Place />} />
+        <Route path="/user/:id" element={<User />} />
+      </Routes>
+      </div>
+    </div>
   </BrowserRouter>
 );
