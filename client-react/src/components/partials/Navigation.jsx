bfbd24f99e1c848ccc84bb4782ddc63a4fa9a4a1
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-10-04 21:35:28 -0400

    Add Map

--- client-react/src/components/partials/Navigation.jsx
+++ client-react/src/components/partials/Navigation.jsx
@@ -37,9 +37,10 @@ export default function Navigation() {
         <img style={appIconStyle} src={appIcon} alt="Logo" />
         {/*<Button fullWidth={true} sx={{ ...navBtnStyle, ...userBtnStyle }} href="/user/1" size="small"><i className="bi bi-person-circle"></i></Button>*/}
         <Button fullWidth={true} sx={{ ...navBtnStyle, ...userBtnStyle }} href="/user/1" size="small"><img style={userIconStyle} src={userIcon} alt="Avatar" /></Button>
-        <Button fullWidth={true} sx={navBtnStyle} href="/" size="small"><i className="bi bi-house-fill"></i></Button>
-        <Button fullWidth={true} sx={navBtnStyle} href="/survey" size="small"><i className="bi bi-suitcase2-fill"></i></Button>
-        <Button fullWidth={true} sx={navBtnStyle} href="/schedule/1" size="small"><i className="bi bi-calendar2-week-fill"></i></Button>
+        <Button fullWidth={true} sx={navBtnStyle} href="/" size="small"><i className="bi bi-house-door"></i></Button>
+        <Button fullWidth={true} sx={navBtnStyle} href="/survey" size="small"><i className="bi bi-suitcase2"></i></Button>
+        <Button fullWidth={true} sx={navBtnStyle} href="/schedule/1" size="small"><i className="bi bi-calendar-week"></i></Button>
+        <Button fullWidth={true} sx={navBtnStyle} href="/map" size="small"><i className="bi bi-geo-alt"></i></Button>
         <Button fullWidth={true} sx={navBtnStyle} href="/place/1" size="small">1</Button>
         <Button fullWidth={true} sx={navBtnStyle} href="/google" size="small">2</Button>
         <Button fullWidth={true} sx={navBtnStyle} href="/duration" size="small">3</Button>
