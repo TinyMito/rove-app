50d13bd3afea035a0967e87c04d5574d5c8c21be
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-10-05 03:15:49 -0400

    Refactor Header and Nav

--- client-react/src/components/partials/Header.jsx
+++ client-react/src/components/partials/Header.jsx
@@ -3,11 +3,15 @@ import { Button } from '@mui/material';
 
 export default function Header() {
 
+  const sloganStyle = {
+    color: '#FFF'
+  }
+
   return (
     <div className="Header">
       <div>
         <h2>Welcome -user_name-!</h2>
-        <h1>It's time to pick you next travel destination!</h1>
+        <h1 style={sloganStyle}>It's time to pick you next travel destination!</h1>
       </div>
     </div>
   );
