25ff77713962e5e38f8036f9e257e1dcaa4cd9e3
eunsookim1 <92eunsoo@gmail.com>
2023-10-04 19:54:04 -0700

    database update -add google_place_id and usernote

--- server-express/src/db/schema/create-tables.sql
+++ server-express/src/db/schema/create-tables.sql
@@ -40,6 +40,7 @@ CREATE TABLE schedules (
   end_date VARCHAR(10),
   user_id INT REFERENCES users(id),
   destination_id INT REFERENCES destinations(id)
+  google_place_id VARCHAR(255)
 );
 
 CREATE TABLE trips (
@@ -50,6 +51,7 @@ CREATE TABLE trips (
   schedule_id INT REFERENCES schedules(id),
   date VARCHAR(10),
   start_time VARCHAR(5),
-  end_time VARCHAR(5)
+  end_time VARCHAR(5),
+  user_note TEXT DEFAULT 'Write your own trip notes here!'
 );
 
