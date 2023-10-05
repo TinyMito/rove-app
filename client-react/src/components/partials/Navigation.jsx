f9d88452b940037e7395777b561fae59b61eec0f
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-10-05 12:35:31 -0400

    Adjust Nav buttons

--- client-react/src/components/partials/Navigation.jsx
+++ client-react/src/components/partials/Navigation.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Button } from '@mui/material';
 import appIcon from '../../assets/images/logo.png';
 import userIcon from '../../assets/images/avatar.png';
@@ -7,13 +8,13 @@ export default function Navigation() {
   const appIconStyle = {
     padding: '20px', 
     width: '80px',
-    filter: 'drop-shadow(0.1rem 0.1rem 0.5rem rgba(80, 80, 80, 0.4))'
+    filter: 'drop-shadow(0.1rem 0.1rem 0.5rem rgba(80, 80, 80, 0.5))'
   }
 
   const userIconStyle = {
     padding: '20px', 
     width: '65px',
-    filter: 'drop-shadow(0.1rem 0.1rem 0.5rem rgba(80, 80, 80, 0.4))',
+    filter: 'drop-shadow(0.1rem 0.1rem 0.5rem rgba(80, 80, 80, 0.5))',
     borderRadius: '1em'
   }
 
@@ -34,10 +35,11 @@ export default function Navigation() {
   return (
     <div className="Navigation">
       <div>
-        <img style={appIconStyle} src={appIcon} alt="Logo" />
+        {/*<Link to="/"><img style={appIconStyle} src={appIcon} alt="Logo" /></Link>*/}
+        <Button fullWidth={true} sx={navBtnStyle} href="/"><img style={appIconStyle} src={appIcon} alt="Logo" /></Button>
         {/*<Button fullWidth={true} sx={{ ...navBtnStyle, ...userBtnStyle }} href="/user/1" size="small"><i className="bi bi-person-circle"></i></Button>*/}
         <Button fullWidth={true} sx={{ ...navBtnStyle, ...userBtnStyle }} href="/user/1" size="small"><img style={userIconStyle} src={userIcon} alt="Avatar" /></Button>
-        <Button fullWidth={true} sx={navBtnStyle} href="/" size="small"><i className="bi bi-house-door"></i></Button>
+        {/*<Button fullWidth={true} sx={navBtnStyle} href="/" size="small"><i className="bi bi-house-door"></i></Button>*/}
         <Button fullWidth={true} sx={navBtnStyle} href="/duration" size="small"><i className="bi bi-suitcase2"></i></Button>
         <Button fullWidth={true} sx={navBtnStyle} href="/schedule/1" size="small"><i className="bi bi-calendar-week"></i></Button>
         <Button fullWidth={true} sx={navBtnStyle} href="/map" size="small"><i className="bi bi-geo-alt"></i></Button>
