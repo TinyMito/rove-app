cd593695876cd9f792dba16330d575fd789aef3b
eunsookim1 <92eunsoo@gmail.com>
2023-10-05 18:36:27 -0700

    className update

--- client-react/src/components/Schedule/Schedule.jsx
+++ client-react/src/components/Schedule/Schedule.jsx
@@ -14,66 +14,45 @@ export const Schedule = (props) => {
   const { start_date, end_date } = schedule || {};
   const { data, deleteTrip } = useTimeLine({ id, date: dates[currentDay] });
 
-  const page_heading = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'baseline',
-    color: 'rgb(58, 57, 57)',
-    borderBottom: '5px solid black',
-  };
-
-  const schedule_heading = {
-    display: 'flex',
-    justifyContent: 'right',
-    alignItems: 'center'
-  };
-
-  const travel_dates = {
-    color: 'rgb(58, 57, 57)',
-    display: 'flex',
-    justifyContent: 'center'
-  };
-
   return (
     <div className="box">
-    <div className="flex-row">
+      <div className="flex-row">
         <Navigation />
         <div className="flex-column">
           <Header />
-
-    <div className="body">
-      <title >Vancouver</title>
-      <div id="root"></div>
-
-      <div style={page_heading}>
-        <h1>Vancouver</h1>
-        <div className="schedule-heading">
-          <h2 style={{ marginRight: '0.5em' }}>DAY:</h2>
-          <Days
-            daysCount={totalDays}
-            handleChange={handleSetDay}
-            currentDay={currentDay + 1}
-          />
+          <div className="body">
+            <title >Vancouver</title>
+            <div id="root"></div>
+            <div className="page-heading">
+              <h1>Vancouver</h1>
+              <div className="schedule-heading">
+                <h2 style={{ marginRight: '0.5em' }}>DAY:</h2>
+                <Days
+                  daysCount={totalDays}
+                  handleChange={handleSetDay}
+                  currentDay={currentDay + 1}
+                />
+              </div>
+            </div>
+            <h2 className="travel-dates">
+              {start_date ? `${start_date} ~ ${end_date}` :  'Loading...'}
+            </h2>
+            <section className="itinerary_day">
+              <Button fullWidth={true} sx={{ fontSize: '50px' }} href="#" size="small">
+                <i className="bi bi-plus"></i>
+              </Button>
+              <ScheduleTimeLine
+                data={data}
+                deleteTrip={deleteTrip}
+                className="schedule-card"
+              />
+            </section>
+           </div>
         </div>
       </div>
-      <h2 className="travel-dates">
-        {start_date ? `${start_date} ~ ${end_date}` :  'Loading...'}
-      </h2>
-
-      <section className="itinerary_day">
-        <Button fullWidth={true} sx={{ fontSize: '50px' }} href="#" size="small">
-          <i className="bi bi-plus"></i>
-        </Button>
-
-        <ScheduleTimeLine
-          data={data}
-          deleteTrip={deleteTrip}
-          className="schedule-card"
-        />
-      </section>
-      </div>
+    </div>
   );
 
 }
 
-// travel time - random math time give it 15min, 30min, 45min, etc.
+{/* // travel time - random math time give it 15min, 30min, 45min, etc. */}
