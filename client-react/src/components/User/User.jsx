35e67698ec32c60028927721f144ed429c474e54
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-10-07 00:45:25 -0400

    Update user page.

--- client-react/src/components/User/User.jsx
+++ client-react/src/components/User/User.jsx
@@ -5,37 +5,50 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+// User CSS
+import '../../styles/User.scss'
+
+// Layout
 import Navigation from '../partials/Navigation';
 import Header from '../partials/Header';
 
-import MyTrips from './MyTrips';
+// Components for User
+import MyScheduleList from './MyScheduleList';
+import TripSuggestion from './TripSuggestion';
 
-export default function Detail() {
+export default function User() {
   // Get url id parameter
   const { id } = useParams();
-  const [ trips, setTrips ] = useState([]);
-
-  console.log("JSON",trips)
-
-  const page_heading = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'baseline',
-    borderBottom: '5px solid #71B1F8',
-  };
-
+  const [schedules, setScheduleList] = useState([]);
+  const [suggestedTrips, setSuggestedTrips] = useState([]);
+  const [userName, setUserName] = useState([]);
+  
   useEffect(() => {
     const apiUser = `/api/user/${id}`;
 
     axios.get(apiUser)
       .then((res) => {
-        setTrips(res.data);
+        setScheduleList(res.data.schedule);
+        setSuggestedTrips(res.data.suggestedTrip);
+        setUserName(res.data.schedule[0].first_name);
       })
       .catch((err) => {
-        setTrips({ error: err.message });
+        setScheduleList({ error: err.message });
+        setSuggestedTrips({ error: err.message });
+        setUserName({ error: err.message });
       });
   }, [id]);
-  
+
+  function randomPicker(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  const selectedTrips = randomPicker(suggestedTrips).slice(0,8);
+
   return (
     <div className="box">
     <div className="flex-row">
@@ -44,15 +57,17 @@ export default function Detail() {
           <Header />
 
     <div className="body">
-      <h1>Welcome {trips.first_name} {trips.last_name}!</h1>
-      <div style={page_heading}><h1>Trip Suggestion</h1></div>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-
+      <h2>{userName}'s profile</h2>
+      <div className="page-heading"><h1>Trip Suggestion</h1></div>
+      <div className="item-list">
+        {selectedTrips.map((item) => (
+          <TripSuggestion key={item.id} trip={item}/>
+        ))}
       </div>
-      <div style={page_heading}><h1>My Schedule</h1></div>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {trips.map((trip) => (
-          <MyTrips key={trip.id} trips={trip}/>
+      <div className="page-heading"><h1>My Schedule</h1></div>
+      <div className="item-list">
+        {schedules.map((item) => (
+          <MyScheduleList key={item.id} schedule={item}/>
         ))}
       </div>
     </div>
--- server-express/src/db/queries/user.js
+++ server-express/src/db/queries/user.js
@@ -4,26 +4,45 @@ const dbQuery = function (query) {
   return (
     db.query(query.string, query.params)
       .then(res => res.rows)
+      .catch(err => {
+        console.error("Database Error:", err);
+        throw err;
+      })
   );
 };
 
 const userScheduleQuery =
   `
-  SELECT schedules.id, schedules.start_date, schedules.end_date, schedules.user_id, 
+  SELECT DISTINCT ON (schedules.id, schedules.start_date, schedules.end_date)
+  schedules.id, schedules.start_date, schedules.end_date, schedules.user_id, 
   users.first_name, users.last_name, destinations.name, destinations.thumbnail_img_url, destinations.cover_photo_url
   FROM schedules 
   JOIN users ON users.id = schedules.user_id
   JOIN destinations ON destinations.id = schedules.destination_id
   WHERE schedules.user_id = $1
-  ORDER BY schedules.user_id, destinations.name;
+  ORDER BY schedules.start_date;
   `;
 
 const getUserSchedule = (userId) => {
   const query = {
     string: userScheduleQuery,
-    params: [userId],
+    params: [userId]
   };
   return dbQuery(query);
 };
 
-module.exports = { getUserSchedule };
+const userSuggestedTripQuery =
+  `
+  SELECT *
+  FROM places;
+  `;
+
+const getUserSuggestedTrip = () => {
+  const query = {
+    string: userSuggestedTripQuery
+  };
+  return dbQuery(query);
+};
+
+
+module.exports = { getUserSchedule, getUserSuggestedTrip };
