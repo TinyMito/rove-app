c564832d3c7da5bb39862e44c6100eb0d89f8f27
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-10-09 00:55:34 -0400

    Map layout

--- client-react/src/components/MapData.jsx
+++ client-react/src/components/MapData.jsx
@@ -1,30 +1,55 @@
+// MUI Components
+import { Card, CardActions, CardContent, CardMedia, Button, Grid, Typography, useMediaQuery, Rating } from '@mui/material';
+
 import React from "react";
 import { MapContainer as Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./MapData.css";
 import { useLocation } from "react-router-dom";
 
+// GLOBAL DATA: Import GlobalData function
+import { globalData } from '../GlobalData';
+
+// Import Navigation & Header
+import Navigation from './partials/Navigation';
+import Header from './partials/Header';
+
 export default function MapData() {
+  // GLOBAL DATA: Add the useState const from globalData, ie. userData.id, userData.firstname etc
+  const { userData, setUserData } = globalData();
+
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const latitude = parseFloat(queryParams.get("lat")) || 51.505;
   const longitude = parseFloat(queryParams.get("lng")) || -0.09;
 
   return (
-    <div className="centered-square-map">
-      <Map
-        center={[latitude, longitude]}
-        zoom={13}
-        style={{ width: "100%", height: "100%" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          maxZoom={19}
-        />
-        <Marker position={[latitude, longitude]}>
-          <Popup>Selected Place</Popup>
-        </Marker>
-      </Map>
-    </div>
-  );
+    <div className="box"> 
+      <div className="flex-row">
+        <Navigation loggedIn={userData.loggedIn} userId={userData.id} userImg={userData.userImg} />
+        <div className="flex-column">
+          <div styles="padding: 0; margin: 0;">
+            {/* Your codes start here */}
+
+              <div className="centered-square-map">
+                <Map
+                  center={[latitude, longitude]}
+                  zoom={13}
+                  style={{ width: "100%", height: "100%" }}
+                >
+                  <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    maxZoom={19}
+                  />
+                  <Marker position={[latitude, longitude]}>
+                    <Popup>Selected Place</Popup>
+                  </Marker>
+                </Map>
+              </div>
+
+            {/* Your codes end here */}
+            </div>
+        </div>
+      </div>
+    </div>  );
 }
