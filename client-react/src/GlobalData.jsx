31042c96c8fc7f30118f47a32704b792c2e9e7dc
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-10-09 22:50:10 -0400

    Update GlobalData for express session login and fetch userdata after login, logout, register.

--- client-react/src/GlobalData.jsx
+++ client-react/src/GlobalData.jsx
@@ -1,13 +1,17 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 const UserContext = createContext();
 
 export function DataProvider({ children }) {
   const [userData, setUserData] = useState({
-    id: 1,
-    userName: 'Kevin',
-    userImg: 'avatar00.png',
-    loggedIn: true,
+    id: null,
+    userFirst: null,
+    userLast: null,
+    userName: null,
+    userEmail: null,
+    userImg: null,
+    loggedIn: false,
     scheduleId: null,
     scheduleStartDate: null,
     scheduleEndDate: null,
@@ -15,15 +19,40 @@ export function DataProvider({ children }) {
   });
 
   useEffect(() => {
-    console.log('userData:', userData);
-  }, [userData]);
+    // Fetch user session data and set it in the state
+    fetchUserData();
+  }, []);
+
+  const fetchUserData = () => {
+    axios
+      .get('/api/getUserSession')
+      .then((response) => {
+        setUserData({
+          id: response.data.userId,
+          userFirst: response.data.userFirst,
+          userLast: response.data.userLast,
+          userName: response.data.userAlias,
+          userEmail: response.data.userEmail,
+          userImg: response.data.userProfile,
+          loggedIn: true, // Assuming successful login updates loggedIn
+          scheduleId: response.data.scheduleId,
+          scheduleStartDate: response.data.scheduleStartDate,
+          scheduleEndDate: response.data.scheduleEndDate,
+          destinationId: response.data.destinationId
+        });
+      })
+      .catch((error) => {
+        console.error('Error getting user data:', error);
+      });
+  };
+
+  console.log('userData:', userData);
 
   return (
-    <UserContext.Provider value={{ userData, setUserData }}>
+    <UserContext.Provider value={{ userData, setUserData, fetchUserData }}>
       {children}
     </UserContext.Provider>
   );
-
 }
 
 export function globalData() {
