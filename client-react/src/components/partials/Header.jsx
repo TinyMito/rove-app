821bed3aa1278cf93f4a5bc1036764f558cdcd5e
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-10-09 22:58:41 -0400

    Rebuilt Header and Nav components

--- client-react/src/components/partials/Header.jsx
+++ client-react/src/components/partials/Header.jsx
@@ -2,12 +2,17 @@ import { Link } from "react-router-dom";
 import { Button } from '@mui/material';
 import '../../styles/Header.scss';
 
-export default function Header({userName}) {
+// GLOBAL DATA: Import GlobalData function
+import { globalData } from '../../GlobalData';
+
+export default function Header({isAuthenticated, userName}) {
+  // GLOBAL DATA: Add the useState const from globalData, ie. userData.id, userData.firstname etc
+  const { userData, setUserData, fetchUserData } = globalData();
 
   return (
     <div className="Header">
       <div>
-        <h2 className="sloganStyle">Welcome {userName}!</h2>
+        <h2 className="sloganStyle">Welcome {isAuthenticated || userData.id !== undefined ? `${userName}!` : "Traveller!"}</h2>
         <h1 className="sloganStyle">It's time to pick your next travel destination!</h1>
       </div>
     </div>
