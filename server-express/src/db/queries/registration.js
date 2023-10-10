ffb9ac2d7acb2d992fcb2ad583c62222a0cad6df
TinyMito <75095713+TinyMito@users.noreply.github.com>
2023-10-09 22:53:53 -0400

    Updated Registration page for full details.

--- server-express/src/db/queries/registration.js
+++ server-express/src/db/queries/registration.js
@@ -2,10 +2,13 @@ const db = require('../connection');
 
 // Function to insert a new user
 const createUser = async (user) => {
-  const result = await db.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *', [
+  const result = await db.query('INSERT INTO users (first_name, last_name, username, email, password, profile_thumbnail_img) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;', [
+    user.firstname,
+    user.lastname,
     user.username,
     user.email,
     user.password,
+    user.avatar
   ]);
   return result.rows[0];
 };
